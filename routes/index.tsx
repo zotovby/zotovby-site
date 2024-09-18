64abf8636cfd93df51f5fef104879950526ade6e
zotovby <s.zotov@swag42.com>
2024-09-18 23:58:14 +0200

    fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-5xl font-bold">Hi!👋</h1>
         <div class="pt-10">
-        <h2 class="text-3xl" > 🤙💬<a href="https://t.me/zotovby" class="no-underline hover:underline" >Telegram</a></h2>
-        <h2 class="text-3xl pt-2"> 📷👨‍🎨️ <a href="https://www.instagram.com/zotovby/" class="no-underline hover:underline" >Instagram</a> </h2>
+        <h2 class="text-3xl" >🤙💬 <a href="https://t.me/zotovby" class="no-underline hover:underline" >Telegram</a></h2>
+        <h2 class="text-3xl pt-2">📷👨‍🎨️ <a href="https://www.instagram.com/zotovby/" class="no-underline hover:underline" >Instagram</a> </h2>
         </div>
       </div>
     </div>
